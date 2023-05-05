024cb71aa66512edfa36e7d2f695493e1ef70ac3
YOKANA <yokana.adita@outlook.com>
2023-05-05 14:34:23 +0700

    grantingAccess

--- Com.Efrata.Service.Core.Lib/Migrations/20220916005201_Remove_NPWP_GarmentForwarder.cs
+++ Com.Efrata.Service.Core.Lib/Migrations/20220916005201_Remove_NPWP_GarmentForwarder.cs
@@ -4,22 +4,22 @@ using System.Collections.Generic;
 
 namespace Com.Efrata.Service.Core.Lib.Migrations
 {
-    public partial class Remove_NPWP_GarmentForwarder : Migration
-    {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "NPWP",
-                table: "GarmentForwarders");
-        }
+    //public partial class Remove_NPWP_GarmentForwarder : Migration
+    //{
+    //    protected override void Up(MigrationBuilder migrationBuilder)
+    //    {
+    //        migrationBuilder.DropColumn(
+    //            name: "NPWP",
+    //            table: "GarmentForwarders");
+    //    }
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<string>(
-                name: "NPWP",
-                table: "GarmentForwarders",
-                maxLength: 50,
-                nullable: true);
-        }
-    }
+    //    protected override void Down(MigrationBuilder migrationBuilder)
+    //    {
+    //        migrationBuilder.AddColumn<string>(
+    //            name: "NPWP",
+    //            table: "GarmentForwarders",
+    //            maxLength: 50,
+    //            nullable: true);
+    //    }
+    //}
 }
---
+++ Com.Efrata.Service.Core.WebApi/Controllers/v1/UploadControllers/MenusUploadController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Com.Efrata.Service.Core.Lib;
+using Com.Efrata.Service.Core.Lib.Services;
+using Com.Efrata.Service.Core.WebApi.Helpers;
+using Com.Efrata.Service.Core.Lib.Models;
+using Com.Efrata.Service.Core.Lib.ViewModels;
+using Com.Efrata.Service.Core.Lib.Models.Account_and_Roles;
+
+namespace Com.Efrata.Service.Core.WebApi.Controllers.v1.UploadControllers
+{
+    [Produces("application/json")]
+    [ApiVersion("1.0")]
+    [Route("v{version:apiVersion}/master/upload-menus")]
+    public class MenusUploadController : BasicUploadController<MenuService, Menus, MenuViewModel, MenuService.MenuMap, CoreDbContext>
+    {
+        private static readonly string ApiVersion = "1.0";
+        public MenusUploadController(MenuService service) : base(service, ApiVersion)
+        {
+        }
+    }
+}
